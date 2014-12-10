81d3cb521e213b4e9bc9adc9f23f0821be21dee7
Tom Wiesing <tkw01536@gmail.com>
2014-12-10 14:23:50 +0100

    Added a mailing function

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # USGMailer
 
 A really simple mailer for the USG.
+
+## What does it use?
+
+### Server Side
+    * [NodeJS](http://nodejs.org/)
+    * [Express](http://expressjs.com/)
+    * [nodemailer](http://www.nodemailer.com/)
+    * [html-to-js](https://www.npmjs.com/package/html-to-text)
+### Client Side
+    * [jQuery](http://jquery.com)
+    * [Bootstrap](http://getbootstrap.com/)
+    * [bootstrap-wysiwyg](https://github.com/mindmup/bootstrap-wysiwyg/)
---
+++ src/mail.js
@@ -0,0 +1,47 @@
+var
+    nodemailer = require('nodemailer'),
+    smtpTransport = require('nodemailer-smtp-transport'),
+    htmlToText = require('html-to-text'),
+    templates = require("./templates");
+
+
+var transporter = nodemailer.createTransport(smtpTransport({
+    host: "exchange.jacobs-university.de",
+    port: 25
+}));
+
+
+module.exports.sendMail = function(from, to, subject, content, html_content, attachments, callback){
+    if(typeof to !== "string"){
+        var to = to.join(", ");
+    } else {
+        var to = to;
+    }
+
+    return transporter.sendMail({
+        from: from,
+        to: to,
+        subject: subject,
+        text: content,
+        html: html_content
+        attachments: attachments
+    }, callback);
+}
+
+module.exports.expandTemplate = function(template, subject, from, to, htmlContent, attachments, callback){
+    var expandedTemplate = templates.getTemplateIfAvailable(template);
+
+    if(!expandedTemplate){
+        return callback(false, "Template not found! ");
+    }
+
+    if(!expandedTemplate.html){
+        var htmlContent = undefined;
+    } else {
+        var htmlContent = expandedTemplate.html({content: htmlContent});
+    }
+
+    var textContent = expandedTemplate.text({content: htmlToText.fromString(htmlContent, {wordWrap: 130})});
+
+    return module.exports.sendMail(from, to, subject, textContent, htmlContent, attachments, callback);
+}
