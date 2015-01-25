806525b42eed9a0cdea0da04d2f50a9a3583ac7a
Tom Wiesing <tkw01536@gmail.com>
2015-01-25 16:51:45 +0100

    Added an ability to create drafts

--- src/express/routes/02_sender.js
+++ src/express/routes/02_sender.js
@@ -4,183 +4,25 @@ var
   usermodel = require("../../usermodel"),
   templates = require("../../sender/templates.js");
 
-module.exports = function(app, session, path){
-  process.stdout.write(" [DISABLED]");
-  return;
+module.exports = function(app, usermodel, path){
 
-  var get_meta = function(req, res, callback){
-    var avs = templates.getAll();
-
-    for(var i=0;i<avs.length;i++){
-      avs[i] = avs[i].meta;
-    }
-
-    usermodel.getAllowedEmails(req.session.user, function(mails){
-      callback({
-        "fromMails": mails,
-        "mailSuffix": config.getConfig().mail,
-        "templates": avs
-      });
-    });
-  };
-
-  var newMail = function(req, res){
-    return {
-      "subject": "New mail",
-      "id": usermodel.createNewDraftId(),
-
-      "from": req.session.user,
-      "template": "simple",
-
-      "to": [],
-      "cc": [],
-      "bcc": [],
-
-      "text": "New mail!"
-    };
-  }
-
-
-  //prepare to send the mail (i. e. validate the data)
-  //callback with data or false
-  var prepareMail = function(req, res, callback){
-
-    try{
-      var data = JSON.parse(req.param("data"));
-    } catch(e){
-      return callback(false);
-    }
-
-    usermodel.getAllowedEmails(req.session.user, function(suc, allowedMails){
-      return mail.tryExpandTemplate(data, allowedMails, callback);
+  var renderMainPage = function(req, res, error){
+    usermodel.drafts.getDrafts(req.session.user, function(success, message){
+      res.render(path("static", "sender", "index.html"), {"user": req.session.user, "message":success?error:message, "drafts": message});
     });
   }
 
-  app.get("/compose/backend/get_meta", session.needUser, function(req, res){
-    get_meta(req, res, function(meta){
-      res.jsonp(meta);
-    });
-  });
-
-  var renderMain = function(req, res, message){
-    usermodel.getDraftsFull(req.session.user, function(success, drafts){
-      if(!success){
-        res.render(path("static", "sender", "index.html"), {"drafts": [], "message": "Something went wrong! Please try again or contact the administrator. ", "user": req.session.user});
-        return;
+  app.get("/new", usermodel.session.needUser, function(req, res){
+    usermodel.drafts.createNewDraft(req.session.user, function(success, message){
+      if(success){
+        res.redirect("/edit/"+message);
+      } else {
+        renderMainPage(req, res, message);
       }
-
-      res.render(path("static", "sender", "index.html"), {"drafts": drafts, "message": message, "user": req.session.user});
-    });
-  }
-
-  var renderCompose = function(req, res, id, message_ok, message_fail){
-    usermodel.getDraft(req.session.user, id, function(success, draft){
-      if(!success){
-        renderMain(req, res, "That draft does not exist. ");
-        return;
-      }
-
-      get_meta(req, res, function(meta){
-        //store some additonal parameters.
-        meta.id = id;
-        meta.user = req.session.user;
-        meta.message_ok = message_ok;
-        meta.message_fail = message_fail;
-        draft.meta = meta;
-
-        res.render(path("static", "sender", "compose.html"), draft);
-      });
-
-    });
-  }
-
-
-  var parsePostDraft = function(req){
-
-    var splitter = function(data){
-      if(data == ""){
-        return [];
-      }
-
-      data = data.split(",").join(";").split(";");
-      for(var i=0;i<data.length;i++){
-        data[i] = data[i].trim();
-      }
-
-      return data;
-    }
-
-    return {
-      "subject": req.param("subjectEmail"),
-
-      "from": req.param("fromEmail"),
-      "template": req.param("templateEmail"),
-
-      "to": splitter(req.param("toEmail")),
-      "cc": splitter(req.param("ccEmail")),
-      "bcc": splitter(req.param("bccEmail")),
-
-      "text": req.param("textEmail"),
-      "id": req.param("emailId")
-    }
-  }
-
-  var saveDraft = function(user, id, data, callback){
-    usermodel.getDrafts(user, function(s, drafts){
-      if(!s || drafts.indexOf(id) == -1){
-        callback(false);
-      }
-
-      usermodel.setDraft(user, id, data, function(success, message){
-        callback(success, message);
-      });
-    });
-  }
-
-  app.get("/compose/", session.needUser, function(req, res){
-    //create a new mail
-    var mail = newMail(req, res);
-    usermodel.setDraft(req.session.user, mail.id, mail, function(){
-      res.redirect("/compose/"+mail.id);
-    });
+    })
   });
 
-  app.get("/compose/:id", session.needUser, function(req, res){
-    var id = req.params.id;
-    renderCompose(req, res, id);
+  app.get("/", usermodel.session.needUser, function(req, res){
+    renderMainPage(req, res);
   });
-
-  app.post("/compose/:id", session.needUser, function(req, res){
-    var action = req.param("action");
-
-    var draft = parsePostDraft(req);
-    console.log(draft);
-
-    if(action == "Save Draft"){
-      saveDraft(req.session.user, draft.id, draft, function(success){
-        if(!success){
-          renderCompose(req, res, draft.id, undefined, "Did not save draft");
-        } else {
-          renderCompose(req, res, draft.id, "Draft saved. ");
-        }
-      });
-    } else {
-      renderMain(req, res, "Unknown action. ");
-    }
-  });
-
-  app.get("/compose/backend/store_draft", session.needUser, function(req, res){
-    var id = req.param("id");
-    var data = req.param("data");
-
-    saveDraft(req.session.user, id, data, function(success, message){
-      res.jsonp({"success": success, "message": message?message.message:undefined});
-      res.end();
-    });
-  });
-
-  app.get("/", session.needUser, function(req, res){
-    renderMain(req, res);
-  });
-
 }
--- src/usermodel/components/03_drafts.js
+++ src/usermodel/components/03_drafts.js
@@ -1,5 +1,7 @@
 var
-  uuid = require("node-uuid");
+  uuid = require("node-uuid"),
+  extend = require("extend"),
+  templates = require("../../sender/templates.js");
 
 module.exports = function(usermodel){
   usermodel.drafts = {};
@@ -9,13 +11,13 @@ module.exports = function(usermodel){
       if(success){
 
         //extract the draft ids
-        var draftsIds = [];
+        var draftIds = [];
         for(var i=0;i<message.length;i++){
-          draftsIds.push(message[i].id);
+          draftIds.push(message[i].id);
         }
 
         //and send them
-        callback(true, draftIds)
+        callback(true, draftIds);
       } else {
         callback(false, message);
       }
@@ -29,7 +31,7 @@ module.exports = function(usermodel){
         //search for the draft and find it.
         for(var i=0;i<message.length;i++){
           if(message[i].id == id){
-            callback(true, message[i].data);
+            usermodel.drafts.buildDraft(user, message[i].data, callback);
             return;
           }
         }
@@ -43,17 +45,29 @@ module.exports = function(usermodel){
   }
 
   usermodel.drafts.getDrafts = function(user, callback){
+    //extract the drafts
+    var drafts = [];
+    var rawDrafts = [];
+
+    var step = function(i, length){
+      if(i < length){
+        usermodel.drafts.buildDraft(user, rawDrafts[i].data, function(success, message){
+          if(success){
+            drafts.push({id: rawDrafts[i].id, data: message});
+            step(i+1, length);
+          } else {
+            callback(false, message);
+          }
+        });
+      } else {
+        callback(true, drafts);
+      }
+    }
+
     usermodel.users.getDrafts(user, function(success, message){
       if(success){
-
-        //extract the drafts
-        var drafts = [];
-        for(var i=0;i<message.length;i++){
-          drafts.push(message[i].data);
-        }
-
-        //and send them
-        callback(true, drafts)
+        rawDrafts = message;
+        step(0, rawDrafts.length);
       } else {
         callback(false, message);
       }
@@ -61,29 +75,36 @@ module.exports = function(usermodel){
   }
 
   usermodel.drafts.setDraft = function(user, id, draft, callback){
-    usermodel.users.getDrafts(user, function(success, message){
+    usermodel.drafts.buildDraft(user, draft, function(success, message){
       if(success){
+        var draft = message;
+        usermodel.users.getDrafts(user, function(success, message){
+          if(success){
 
-        var done = false;
+            var done = false;
 
-        //search for the draft and insert it.
-        for(var i=0;i<message.length;i++){
-          if(message[i].id == id){
-            message[i].data = draft;
-            done = true;
-            break;
+            //search for the draft and insert it.
+            for(var i=0;i<message.length;i++){
+              if(message[i].id == id){
+                message[i].data = draft;
+                done = true;
+                break;
+              }
+            }
+
+            if(!done){
+              message.push({
+                "id": id,
+                "data": draft
+              });
+            }
+
+            //set the drafts
+            usermodel.users.setDrafts(user, message, callback);
+          } else {
+            callback(false, message);
           }
-        }
-
-        if(!done){
-          message.push({
-            "id": id,
-            "data": data
-          });
-        }
-
-        //set the drafts
-        usermodel.users.setDrafts(user, message, callback);
+        });
       } else {
         callback(false, message);
       }
@@ -98,6 +119,10 @@ module.exports = function(usermodel){
             callback(true, true);
             return;
           }
+        }if(!draft.hasOwnProperty("subject") || typeof draft.subject !== "string"){
+          cleanDraft.subject = "";
+        } else {
+          cleanDraft.subject = draft.subject;
         }
 
         //hmm, can't find it.
@@ -129,9 +154,123 @@ module.exports = function(usermodel){
     });
   }
 
+  usermodel.drafts.buildDraft = function(user, draft, callback){
+    usermodel.users.getAllowedEmails(user, function(success, message){
+      if(success){
+        var availableEmails = message;
+        usermodel.drafts.getAvailableTemplates(function(success, message){
+          if(success){
+            var cleanDraft = {};
+            var availableTemplates = message;
+
+            if(typeof draft !== "object"){
+              callback(false, "Draft must be an object. ");
+              return;
+            }
+
+            //TODO: to, cc, bcc
+
+            //validate subject
+            if(!draft.hasOwnProperty("subject") || typeof draft.subject !== "string"){
+              cleanDraft.subject = "";
+            } else {
+              cleanDraft.subject = draft.subject;
+            }
+
+            //check from
+            if(!draft.hasOwnProperty("from") || typeof draft.from !== "string" || availableEmails.indexOf(draft.from) == -1){
+              cleanDraft.from = availableEmails[0]; //from myself.
+            } else {
+              cleanDraft.from = draft.from;
+            }
+
+            //check content
+            if(!draft.hasOwnProperty("content") || typeof draft.content !== "string"){
+              cleanDraft.content = "";
+            } else {
+              cleanDraft.content = draft.content;
+            }
+
+            //check template, TODO: Implement available templates.
+            if(!draft.hasOwnProperty("template") || typeof draft.template !== "string" || availableTemplates.indexOf(draft.template) == -1){
+              cleanDraft.template = availableTemplates[0];
+            } else {
+              cleanDraft.template = draft.template;
+            }
+
+            //message content-type, either text or html.
+            if(!draft.hasOwnProperty("content_type") || typeof draft.content_type !== "string" || ["html", "text"].indexOf(draft.content_type) == -1){
+              cleanDraft.content_type = "html"; //default content type is html.
+            } else {
+              cleanDraft.content_type = draft.content_type;
+            }
+
+            //TODO: Render Draft here.
+            var theTemplate = templates.getTemplateIfAvailable(cleanDraft.template);
+            var theDraft = theTemplate[cleanDraft.content_type] || theTemplate["text"] || theTemplate["html"];
+
+            //render the draft
+            try{
+              //render the draft as a copy of this object.
+              cleanDraft.content_cooked = theDraft(JSON.parse(JSON.stringify(cleanDraft)));
+            } catch(e){
+              callback(false, "Unable to render template '"+cleanDraft.template+"': "+e.message);
+              return;
+            }
+
+            //The end.
+            callback(true, cleanDraft);
+          } else {
+            callback(false, message);
+          }
+        });
+      } else {
+        callback(false, message);
+      }
+    });
+  }
+
+  usermodel.drafts.getAvailableTemplates = function(callback){
+    var templData = templates.getAll();
+    var templateNames = [];
+
+    for(var i=0;i<templData.length;i++){
+      templateNames.push(templData[i].meta.title);
+    }
+
+    callback(true, templateNames);
+  }
+
+  usermodel.drafts.createNewDraft = function(user, callback){
+    usermodel.drafts.createNewDraftId(user, function(success, message){
+      var id = message;
+      if(success){
+        usermodel.drafts.setDraft(user, message, {}, function(success, message){
+          if(success){
+            callback(true, id);
+          } else {
+            callback(false, message);
+          }
+        });
+      } else {
+        callback(false, message);
+      }
+    });
+
+  }
+
   usermodel.drafts.createNewDraftId = function(user, callback){
-    //TODO: Make this id a bit smarter.
-    //Maybe just have a counter and always check which ones already exist.
-    callback(true, uuid.v1()+uuid.v4()); 
+    usermodel.drafts.getDraftIds(user, function(success, message){
+      if(success){
+        var newId;
+        while(message.indexOf(newId = uuid.v1()+uuid.v4()) != -1){
+          //do nothing.
+        }
+
+        callback(true, newId);
+      } else {
+        callback(false, message)
+      }
+    });
   }
 }
